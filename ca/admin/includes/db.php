3a0e3fda49026ca48df54c4ea2a26917f6ede729
Canderob2005 <51235457+Canderob2005@users.noreply.github.com>
2021-11-22 21:37:23 -0400

    ok

--- ca/admin/includes/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	define("DB_HOST" , "localhost");
-	define("DB_USER" , "u317430902_clasificadospr");
-	define("DB_PASS" , "Camara123*");
-	define("DB_NAME" , "carousel-sql");
-
-	$db = new mysqli(DB_HOST , DB_USER , DB_PASS , DB_NAME);
-	if(!$db){ echo 'An error has occurred <br/>'.mysqli_connect_error(); }
-?>
